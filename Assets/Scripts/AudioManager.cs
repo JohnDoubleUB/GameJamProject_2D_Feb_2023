32fed251dfec34ab944acf4d3597f7b9aa5514cf
JohnDoubleUB <johndoubleub@gmail.com>
2023-02-04 17:20:27 +0000

    Add audio manager and build soundbanks

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager current;
+
+    private void Awake()
+    {
+
+        if (current != null) Debug.LogWarning("Oops! it looks like there might already be a " + GetType().Name + " in this scene!");
+        current = this;
+    }
+
+    public uint AK_PlayEventAt(string eventName, Vector3 pos)
+    {
+        GameObject tempGO = new GameObject($"TempAudio_{eventName}_"); // create the temp object
+        tempGO.transform.position = pos; // set its position
+        uint eventID = AkSoundEngine.PostEvent(eventName, tempGO, (uint)AkCallbackType.AK_EndOfEvent, AK_CallbackFunction, tempGO);
+        tempGO.name += eventID;
+        return eventID;
+    }
+
+    //This will follow the object
+    public uint AK_PlayClipOnObject(string eventName, GameObject in_gameObjectID)
+    {
+        return AkSoundEngine.PostEvent(eventName, in_gameObjectID);
+    }
+
+    private void AK_CallbackFunction(object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        switch (in_type)
+        {
+            case AkCallbackType.AK_EndOfEvent:
+                if (in_cookie is GameObject) Destroy((GameObject)in_cookie);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DRONEFIRE = 4214056565U;
+        static const AkUniqueID DRONEMOVEMENT = 1720237598U;
+        static const AkUniqueID EARTHAMBIENCE = 3287156155U;
+        static const AkUniqueID EARTHAMBIENCESTOP = 1135202491U;
+        static const AkUniqueID EARTHSONG = 848516368U;
+        static const AkUniqueID EARTHSONGSTOP = 2139794104U;
+        static const AkUniqueID EXPLOSION = 13776098U;
+        static const AkUniqueID HYPERDRIVE = 2734475931U;
+        static const AkUniqueID SHIPFIRE = 3203412995U;
+        static const AkUniqueID SHIPRUMBLE = 2117307804U;
+        static const AkUniqueID SHIPWOOSH = 1751418741U;
+        static const AkUniqueID SPACESONG = 673226934U;
+        static const AkUniqueID SPACESONGSTOP = 1397429982U;
+        static const AkUniqueID VA1 = 644359727U;
+        static const AkUniqueID VA2 = 644359724U;
+        static const AkUniqueID VA3 = 644359725U;
+        static const AkUniqueID VA4 = 644359722U;
+        static const AkUniqueID VA5 = 644359723U;
+        static const AkUniqueID VA6 = 644359720U;
+        static const AkUniqueID VA7 = 644359721U;
+        static const AkUniqueID VA8 = 644359718U;
+        static const AkUniqueID VA9 = 644359719U;
+        static const AkUniqueID VA15 = 2767461320U;
+        static const AkUniqueID VAALLCLICKED = 2510255154U;
+        static const AkUniqueID VABIGBEN = 421286007U;
+        static const AkUniqueID VANEWSPAPER = 1508793495U;
+        static const AkUniqueID VAPHONE = 1510088570U;
+        static const AkUniqueID VAPIZZA = 3534406808U;
+        static const AkUniqueID VASHOE = 89164611U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
